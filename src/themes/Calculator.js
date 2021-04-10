0d449f8b26542495b417db52b1de9f51a444176d
cmercado-b <xtian.mercado@outlook.com>
2021-04-10 23:35:05 +0800

    Button Commited

---
+++ src/themes/Calculator.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const calculator = () => {
+    return (
+        <div className="calculator">
+            <a>0</a>
+            <a>1</a>
+            <a>2</a>
+            <a>3</a>
+            <a>4</a>
+            <a>5</a>
+        </div>
+    );
+};
+
+export default userOutput;
