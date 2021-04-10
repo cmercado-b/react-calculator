3848fc68cc8fc0cd14656ea93bfdbb7e41d70035
cmercado-b <xtian.mercado@outlook.com>
2021-04-11 00:47:20 +0800

    Numbers and Operators Added

--- src/themes/Output.js
+++ src/themes/Output.js
@@ -3,13 +3,21 @@ import React from 'react';
 const Output = () => {
 
     const style = {
-        width: '500px',
-        textAlign: 'center',
+        height: '98px',
+        width: '435px',
+        textAlign: 'right',
+        fontSize: '65px',
+        padding: '0 5px',
+        margin: '30px 0 0 0',
+        borderRadius: '5px',
+        color: '#4AF626',
+        border: '4px solid #4AF626',
+        cursor: 'none',
     };
 
     return (
-        <div className="Output">
-           <input type="text" value="Test" disabled/>
+        <div className="Output" style={style}>
+            123456789
         </div>
     );
 };
