1dacee31fd48c98d3a04f28ec23ed6ea5d62bb47
cmercado-b <xtian.mercado@outlook.com>
2021-04-11 22:14:33 +0800

    Deleted Components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
-import Calculator from './themes/Calculator'; 
-import Output from './themes/Output'; 
 
 function App() {
   return (
     <div className="App">
-      <Output />
-      <Calculator />
     </div>
   );
 }
