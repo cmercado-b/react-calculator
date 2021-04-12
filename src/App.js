2f5b808122c2dc87195d985e3cd1fa907694faf9
cmercado-b <xtian.mercado@outlook.com>
2021-04-13 00:24:56 +0800

    "Button Layout Added"

--- src/App.js
+++ src/App.js
@@ -5,10 +5,39 @@ import Button from './Button/Button.js';
 import Display from './Display/Display.js';
 
 function App() {
+
+  const rowStyle = {
+    display: 'flex',
+    width: '100%',
+  };
+
   return (
     <div className="App">
         <Display />
-        <Button />
+          <div style={rowStyle}>
+            <Button />
+            <Button />
+            <Button />
+            <Button />
+          </div>
+          <div style={rowStyle}>
+            <Button />
+            <Button />
+            <Button />
+            <Button />
+          </div>
+          <div style={rowStyle}>
+            <Button />
+            <Button />
+            <Button />
+            <Button />
+          </div>
+          <div style={rowStyle}>
+            <Button />
+            <Button />
+            <Button />
+            <Button />
+          </div>
     </div>
   );
 }
--- src/Button/Button.js
+++ src/Button/Button.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
 const button = (props) => {
+
+    const style = {
+        display: 'flex',
+        height: '70px',
+        width: '90px',
+        border: '2px outset #ffff00',
+        color: '#d0d0d0',
+        alignItems: 'center',
+        justifyContent: 'center',
+    };
+
     return (
         <div>
-            test
+           <p style={style}>TEST</p>
         </div>
     );
 };
